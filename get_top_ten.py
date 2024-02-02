942c5660b89572e129e0054ba4b818311d6e732f
Simon <simon@elisya.org>
2024-02-02 07:32:33 +0100

    allow to read from folders

--- get_top_ten.py
+++ get_top_ten.py
@@ -1,6 +1,13 @@
 import pickle
+import argparse
+import os
 
-allstatements=pickle.load(open("variantsbeta.pkl","rb"))
+parser = argparse.ArgumentParser(description='get top ten')
+parser.add_argument('--outdir', action="store", dest='outdir', default='.')
+args = parser.parse_args()
+outdir=args.outdir
+
+allstatements=pickle.load(open(os.path.join(outdir,"variantsbeta.pkl"),"rb"))
 sorted_items = sorted(allstatements.items(), key=lambda item: item[1]['counter'], reverse=True)
 
 for i in range(10):
--- get_top_two_time.py
+++ get_top_two_time.py
@@ -2,16 +2,16 @@ import pickle
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='output to csv')
+parser = argparse.ArgumentParser(description='output top trait')
 parser.add_argument('--outdir', action="store", dest='outdir', default='.')
 args = parser.parse_args()
 outdir=args.outdir
 
 allsteps=pickle.load(open(os.path.join(outdir,"alltstepbeta.pkl"),'rb'))
+allstatements=pickle.load(open(os.path.join(outdir,"variantsbeta.pkl"),"rb"))
 tsteps=len(allsteps)
-maxt=len(allsteps[tsteps-1])
-headers=["traits"+str(i) for i in range(maxt)]
-print(",".join(headers))
-for i in allsteps:
-    print(','.join([str(c) for c in i]))
+for stepi in allsteps:
+    top=stepi.index(max(stepi))
+    print(allstatements[top]["statement"])
+
 
