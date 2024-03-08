7dabcc5bce13f2b76d08b8b2bac9a0083245e155
Simon <simon@elisya.org>
2024-03-08 15:40:02 +0000

    minor edits

--- lastautoExplor.sh
+++ lastautoExplor.sh
@@ -9,10 +9,10 @@ do
     do
 
         echo "doing ${mut} vs ${sel} ===="
-        time ./bin/python3 ./gptCultEv.py -N $N -t $t -k $K --statements statements.txt --modprompt modify_previous_for_more_${mut}.prompt --selprompt select_${sel}.prompt --mutate 'yes' --outdir expK${K}N${N}T${t}_mut${sel}_sel${sel}_GPT3.5 --image "e"  &
+        time ./bin/python3 ./gptCultEv.py -N $N -t $t -k $K --statements statements.txt --modprompt modify_previous_for_more_${mut}.prompt --selprompt select_${sel}.prompt --mutate 'yes' --outdir expK${K}N${N}T${t}_mut${mut}_sel${sel}_GPT3.5 --image "e"  &
         # 
         #selection no mutation
-        time ./bin/python3 ./gptCultEv.py -N $N -t $t -k $K --statements statements.txt --modprompt generatenew_${mut}_health.prompt --selprompt select_${sel}.prompt --outdir expK${K}N${N}T${t}_gennew${sel}_sel${sel}_GPT3.5 --image "e"  
+        time ./bin/python3 ./gptCultEv.py -N $N -t $t -k $K --statements statements.txt --modprompt generatenew_${mut}_health.prompt --selprompt select_${sel}.prompt --outdir expK${K}N${N}T${t}_gennew${mut}_sel${sel}_GPT3.5 --image "e"  
 
     done
 done
