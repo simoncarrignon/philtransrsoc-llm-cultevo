aa84c37cdbc46a0057d231d7913e413e961aca0f
Simon <simon@elisya.org>
2025-07-10 08:06:10 +0100

    correct prompt path

--- chain-llms/lastautoExplor.sh
+++ chain-llms/lastautoExplor.sh
@@ -9,9 +9,9 @@ do
     do
 
         echo "doing ${mut} vs ${sel} ===="
-        time ./bin/python3 ./gptCultEv.py -N $N -t $t -k $K --statements statements.txt --modprompt modify_previous_for_more_${mut}.prompt --selprompt select_${sel}.prompt --mutate 'yes' --outdir expK${K}N${N}T${t}_mut${mut}_sel${sel}_GPT3.5 --image "e"  &
+        time ./bin/python3 ./gptCultEv.py -N $N -t $t -k $K --statements statements.txt --modprompt prompts/modify_previous_for_more_${mut}.prompt --selprompt prompts/select_${sel}.prompt --mutate 'yes' --outdir expK${K}N${N}T${t}_mut${mut}_sel${sel}_GPT3.5 --image "e"  &
         # 
-        #selection no mutation
+        #generation from scratch
         time ./bin/python3 ./gptCultEv.py -N $N -t $t -k $K --statements statements.txt --modprompt generatenew_${mut}_health.prompt --selprompt select_${sel}.prompt --outdir expK${K}N${N}T${t}_gennew${mut}_sel${sel}_GPT3.5 --image "e"  
 
     done
