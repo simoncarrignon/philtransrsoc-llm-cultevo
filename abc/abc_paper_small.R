1a3b22633f29b60605b151131f18fd375d266dae
Simon <simon@elisya.org>
2025-12-08 18:24:52 +0000

    save and reload the prior files

--- abc/abc_paper_small.R
+++ abc/abc_paper_small.R
@@ -1,21 +1,15 @@
 outdir <- commandArgs(trailingOnly = TRUE)[1]#folder where  output of ABC will be written
 experiment <- commandArgs(trailingOnly = TRUE)[2]#folder where the concatenated_files.csv are stored 
-loadsimu=TRUE
+
 
 source("R/model-core.R")
 source("R/model-slots.R")
 source("R/abcrfa.R")
 source("R/metrics.R")
-tmpdir <- outdir
-n=0
-while(dir.exists(file.path(here::here(),tmpdir))){
-    n <- n+1
-    tmpdir <- paste0(outdir,"_",n)
-}
-outdir <- tmpdir
-dir.create(file.path(here::here(),outdir))
-
 set.seed(1234)
+
+ns=50000 #number of simulatoin to be run for the ABC
+tol=.01 # percentage of selected simulation infirst stage of ABC
 m=10
 tstep=100
 N=100
@@ -23,12 +17,61 @@ p0=rep(N/m,m)
 u0="rnorm"
 mu=.1
 
-ns=500000
-e=runif(ns,1,2)
-e=rep(1,ns)
-Js=runif(ns,0,2) 
-betas=runif(ns,0,2) 
-prior <- list(J=Js,beta=betas,e=e)
+loadsimu=TRUE
+prior_file <- file.path(here::here(), outdir, "prior.RDS")
+
+if(!loadsimu){
+    tmpdir <- outdir
+    n=0
+    while(dir.exists(file.path(here::here(),tmpdir))){
+        n <- n+1
+        tmpdir <- paste0(outdir,"_",n)
+    }
+    outdir <- tmpdir
+   dir.create(file.path(here::here(),outdir))
+   e=rep(1,ns)
+   Js=runif(ns,0,2) 
+   betas=runif(ns,0,2) 
+   prior <- list(J=Js,beta=betas,e=e)
+   prior_file <- file.path(here::here(), outdir, "prior.RDS")
+   saveRDS(prior, prior_file)
+}
+
+
+# Load prior from file if loadsimu is TRUE
+if (loadsimu) {
+    if (file.exists(prior_file)) {
+        loaded_prior <- readRDS(prior_file)
+        message("Loaded prior from file.")
+        
+        # Check if the loaded prior needs more entries to match 'NS'
+        if (length(loaded_prior$J) < ns) {
+            missing_count <- ns - length(loaded_prior$J)
+            message(sprintf("Loaded prior has %d fewer entries. Generating additional %d values.", missing_count, missing_count))
+            
+            # Generate additional missing values
+            additional_Js <- runif(missing_count, 0, 2)
+            additional_betas <- runif(missing_count, 0, 2)
+            additional_e <- runif(missing_count, 1, 2)
+
+            # Append to the loaded prior
+            loaded_prior$J <- c(loaded_prior$J, additional_Js)
+            loaded_prior$beta <- c(loaded_prior$beta, additional_betas)
+            
+            # Save the augmented prior back to file
+            saveRDS(loaded_prior, prior_file)
+            message("Augmented prior saved.")
+        }
+        e <- loaded_prior$e
+        Js <- loaded_prior$J
+        betas <- loaded_prior$beta
+        
+        # Set prior to loaded prior
+        prior <- loaded_prior
+    } else {
+        stop("Prior file not found and 'loadsimu' is TRUE.")
+    }
+}
 
 
 metrics <- c(d.sim, d.gap , d.turn, d.min,d.max,d.mean,d.median, d.unique)
@@ -38,7 +81,7 @@ metrics  <-  metrics[-c(4:7)]
 mtr=names(metrics);names(mtr)=mtr
 library(parallel)
 
-burnin=1:2 #discard two first steps which are 
+burnin=1:2 #discard two first steps which are artificially prepared
 models  <-  c("GPT3.5","GPT4","O3MINI","Mistral-7B-Instruct-v0.3")[c(1,4)]
 
 file_results  <- list("Mutate statements"="mut","Generate new statements"="gennew")
@@ -46,7 +89,7 @@ list_alladjustments <- list()
 for(mv in models){
     for( ge in names(file_results)){
        expfilenames <- file.path(here::here(),experiment,paste(mv,file_results[[ge]],"concatenated_files.csv",sep = "_"))
-    print(expfilenames)
+       print(expfilenames)
        allexp <- read.csv(expfilenames)
        expnames <- unique(allexp[,c("Mutation","Selection")])
        exnames <- apply(expnames,1,function(e)paste0(e,collapse="_"))
@@ -63,14 +106,14 @@ for(mv in models){
        cat(paste0("Running ABCRFA inference for: \n ",strat," strategy, using ",length(metrics)," metrics (",paste0(names(metrics),collapse=","),") ========\n"))
        allmetricsallex <- lapply(allexp,function(exp)lapply(metrics,function(f)f(exp)))
     
-       cl <- makeCluster(10,"FORK",outfile=file.path("test",paste0(gsub(" ","_",tolower(strat)), "_log_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".log")))
+       cl <- makeCluster(10,"FORK",outfile=file.path(here::here(),outdir,paste0(gsub(" ","_",tolower(strat)), "_log_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".log")))
        alldismulti <- parLapply(cl,1:ns,function(i) {
            tryCatch(
            {
-               if(i%%5==0)print(i)
+               #if(i%%5==0)print(i)
                if(loadsimu && file.exists(here::here(outdir,paste0(i,"_full.RDS")))){
                    simumetrics=readRDS(file=file.path(here::here(),outdir,paste0(i,"_simumetrics.RDS")))
-                   print("read backup")
+                   #print("read backup")
                }
                else{
 
@@ -121,7 +164,7 @@ for(mv in models){
        ##run adjustment
        alladjustment=lapply(cleaned,function(modelresult){
            artif=modelresult[,disfunc]
-           model.rfa <- abcrfa(obs, param = params[,-3], sumstat = artif , tol = .001)
+           model.rfa <- abcrfa(obs, param = params[,-3], sumstat = artif , tol = tol)
            cor=model.rfa$adj.values[,2]>0 & model.rfa$adj.values[,1]>0
            model.rfa$adj.values=apply(model.rfa$adj.values,2,function(i)i[cor])
            model.rfa
