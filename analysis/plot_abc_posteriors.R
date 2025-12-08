5738dcf968d90f139b79b96d041ef2977776316b
Simon <simon@elisya.org>
2025-12-08 18:25:16 +0000

    way to automatize ABC plots

--- analysis/plot_abc_posteriors.R
+++ analysis/plot_abc_posteriors.R
@@ -1,33 +1,39 @@
-experiment <- commandArgs(trailingOnly = TRUE)[1]
-list_allposteriors <- readRDS(file=file.path(here::here(),experiment,"list_allposteriors.RDS"))
- par(mfrow=c(ceiling(length(list_allposteriors)/2),2),cex=1) 
-if(length(list_allposteriors)==1) par(mfrow=c(1,1),cex=1)
-sel_names <- mut_names <- c("original","efficient","attractive","random")
-cols=palette.colors(n=length(mut_names),palette="Set 2")
-names(cols)=mut_names
-pchs=20+1:length(sel_names) 
-names(pchs)=sel_names
-models  <-  c("GPT3.5","GPT4","O3MINI")
-file_results  <- list("Mutate statements"="mut","Generate new statements"="gennew")
+experiment <- '../testin_abc'
+list_allposteriors <- readRDS(file=file.path(experiment,"list_allposteriors.RDS"))
+
+model
+## in the case that we have multiple 
+names(list_allposteriors[[1]]$alladjustment) <- sub("_newm","newm",names(list_allposteriors[[1]]$alladjustment))
+expnames  <- do.call("rbind.data.frame",strsplit(names(list_allposteriors$gpt3.5_mutate_statements$alladjustment),"_"))
+colnames(expnames) <- c("Mutation","Selection")
+
+
+cols=palette.colors(n=length(unique(expnames$Mutation)),palette="Set 2")
+names(cols)=unique(expnames$Mutation)
+pchs=20+1:length(unique(expnames$Selection)) 
+names(pchs)=unique(expnames$Selection)
+
+models  <-  c("GPT3.5","GPT4","O3MINI")[1]
+models <- ""
 for(mv in models){
-    for( ge in names(file_results)){
-       strat <- paste(mv,ge)
-       if(length(list_allposteriors[[gsub(" ","_",tolower(strat))]])==0)next;
-       tmp <- list_allposteriors[[gsub(" ","_",tolower(strat))]]
-       alladjustment <- tmp$alladjustment
-       allmodes <- tmp$allmodes
-       expnames  <- do.call("rbind.data.frame",strsplit(names(alladjustment),"_"))
-       colnames(expnames) <- c("Mutation","Selection")
-       plot(1,1,ylim=c(0,3),xlim=c(0,2),main=paste(mv,ge),type="n",ylab=expression(beta),xlab="J")
-       for(e in 1:nrow(expnames)){
-           en <- paste0(expnames[e,],collapse="_")
-           try({
-               points(alladjustment[[en]][[1]][,1:2],pch=pchs[[expnames$Selection[[e]]]],bg=cols[[expnames$Mutation[[e]]]])
-           })
-       }
-       uu=legend("topright",legend=mut_names,pt.bg=cols,pch=21,title="Mutation operator",bty="n",cex=.8)
-       legend(x=(uu$rect$left),y=uu$rect$top-(uu$rect$h*1.4),legend=sel_names,pt.bg=0,pch=pchs,title="Selection operator",bty="n",cex=.8)
-       points(do.call("rbind",allmodes),cex=2,pch=pchs[expnames$Selection[1:length(allmodes)]],bg=cols[expnames$Mutation[1:length(allmodes)]],lwd=2)
-    }
+for( ge in names(list_allposteriors)){
+   strat <- paste0(ge)
+   tmp <- list_allposteriors[[gsub(" ","_",tolower(strat))]]
+   allmodes <- tmp$allmodes
+   alladjustment <- tmp$alladjustment
+   png(filename = paste0("ABC_",strat,".png"), width = 2400, height = 2200, res = 300)
+   par(cex=1.1)
+   plot(1,1,ylim=c(0,2),xlim=c(0,2),main=paste(mv,ge),type="n",ylab=expression(beta),xlab="J")
+   for(e in 1:nrow(expnames)){
+       en <- paste0(expnames[e,],collapse="_")
+       try({
+           points(alladjustment[[en]][[1]],pch=pchs[[expnames$Selection[[e]]]],bg=adjustcolor(cols[[expnames$Mutation[[e]]]],.6),lwd=.5)
+       })
+   }
+   points(do.call("rbind",allmodes),cex=2,pch=pchs[expnames$Selection[1:length(allmodes)]],bg=cols[expnames$Mutation[1:length(allmodes)]],lwd=2)
+   uu=legend("topright",legend=unique(expnames$Mutation),pt.bg=cols,pch=21,title="Mutation operator" ,cex=.8,bg="white")
+   legend(x=(uu$rect$left),y=uu$rect$top-(uu$rect$h*1.4),legend=unique(expnames$Selection),pt.bg=0,pch=pchs,title="Selection operator",cex=.8,bg="white")
+   dev.off()
+}
 }
 
