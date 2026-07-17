99269609d3ba70f1810e524edea77f2e5b21c30b
Simon <simon@elisya.org>
2026-07-17 10:35:51 +0100

    minor adjustments of plots

--- scripts-analysis/figures_paper/Figure5.R
+++ scripts-analysis/figures_paper/Figure5.R
@@ -7,15 +7,19 @@ names(pchs)=sel_names
 models<-c("Mistral 7B"= "mistral-7b-instruct-v0.3_generate_new_statements","Qwen2.5 7B"= "qwen2.5-7b-instruct_generate_new_statements","Qwen2.5 7B"="qwen2.5-7b-var1_generate_new_statements","OpenAI ChatGPT3.5"= "gpt3.5_generate_new_statements","Qwen3 8B"= "qwen3-8b-gguf_generate_new_statements")
 
 metrics <- c("anx","negemo","posemo")
-png("Figure5.png",width=2400,height=1000,pointsize=17)
-par(mfrow=c(2,3),mar=c(4,4,2,1),cex=1.2,oma=c(0,0,0,7))
-for(mi in names(models)[c(1,2)]){
-for(me in metrics){
+names(metrics) <- c("anxiety","negative emotions","positive emotions")
+png("Figure5.png",width=2400,height=1300,pointsize=17)
+par(mfrow=c(3,3),mar=c(4,4,2,1),cex=1.2,oma=c(0,0,0,7))
+for(mi in names(models)[c(1,2,4)]){
+for(nme in names(metrics)){
+        me  <-  metrics[nme]
 		m <- models[mi]
-		dt <- read.csv(here::here("scripts-analysis/text_analysis/output/summary_tables/",gsub("generate_new_statements","gennew_concatenated",paste0(m,"_",me,".csv"))))
+        if(me == "posemo")ylim <- c(0,0.25)
+        else ylim <- c(0,0.025)
+		dt <- read.csv(here::here("text_analysis/output/summary_tables/",gsub("generate_new_statements","gennew_concatenated",paste0(m,"_",me,".csv"))))
 		dt$Selection  <-  as.factor(dt$Selection)
 		dt$Mutation  <-  as.factor(dt$Mutation)
-		plot(1,type="n",xaxt="n",pch=20,col=cols["original"],ylim=range(dt[,3]),ylab=me,xlim=c(.7,4.3),main='',xlab="Selection")
+		plot(1,type="n",xaxt="n",pch=20,col=cols["original"],ylab=nme,xlim=c(.7,4.3),main='',xlab="Selection",ylim=ylim)
 		mtext(mi,side=3,line=.5,adj=0,font=2,cex=1.1)
 grid()
 		for(sel in unique(dt$Mutation)) lines(dt[dt$Mutation == sel ,3],type="o",pch=20,col=cols[sel],lwd=4)
--- scripts-analysis/figures_paper/Figure9.R
+++ scripts-analysis/figures_paper/Figure9.R
@@ -5,8 +5,8 @@ alldischanges <- lapply(models[-c(5,3)][c(1,3,2)],function(m)read.csv(paste0("..
 sel_names <- mut_names <- c("original","efficient","attractive","random")
 
 ## getting the posterios for J and beta
-allrep=list(rep01=readRDS(here::here("../scripts-analysis/abc/output/list_allposteriors_rep00.RDS")),
-rep02=readRDS(here::here("../scripts-analysis/abc/output/list_allposteriors_rep01.RDS")))
+allrep=list(rep01=readRDS(here::here("abc/output/list_allposteriors_rep00.RDS")),
+rep02=readRDS(here::here("abc/output/list_allposteriors_rep01.RDS")))
 modmodels=unique(unname(unlist(sapply(allrep,names,USE.NAMES = F))))
 names(modmodels)=c(
   "Mistral 7B",
@@ -55,9 +55,8 @@ for(post in params[2]){
   box(col = "#555555", lwd = 0.9)
   par(xpd=T)
     text(summary_mean_table[,post], summary_mean_table[,column],summary_mean_table[,txt],pos=1,col=ifelse(is.na(cols_op[summary_mean_table[,txt]]),"black",cols_op[summary_mean_table[,txt]]),cex=.8)
-    legend("topright",col=pal(5),pch=20,legend=round(seq(min(secondpost),max(secondpost),length.out=5),2),horiz=T,title=expression(beta),pt.cex=2,bty="n")
+    fields::image.plot(legend.only=T,zlim=c(0,2),col=pal(100),horizontal=T,smallplot=c(.80,.90,.84,.86),axis.args=list(cex.axis=.8,a=c(0,1,2)),legend.args=list(text=expression("content-dependent bias"~(beta)),side=3,line=.4))
 }
 dev.off()
-
 
 
